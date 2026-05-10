670a1349ca0f0e25ef10a712b0c3cdce0712ca73
orieo0201 <60222145@mju.ac.kr>
2026-05-10 13:24:19 +0900

    feat(frontend): Sprint 2 학생 응시 플로우 구현 및 버그 수정
    - 학생 응시 플로우 신규 구현
      - ExamEnterCode: 6자리 시험 코드 입력 페이지 (/exam)
      - ExamEnterStudent: 학번/이름 입력 + 세션 시작 페이지 (/exam/enter)
      - ExamSession: 실시간 시험 응시 페이지 (/exam/session)
      - ExamDone: 제출 완료 페이지 (/exam/done)
    - 학생 응시 API 모듈 추가 (api/exam-session.js)
    - 이탈 감지(useExamGuard) / 전체화면(useFullscreen) 훅 추가
    - 버그 수정: axiosInstance 401 인터셉터, LOCK_UNAVAILABLE, heartbeat 에러 처리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import Dashboard from './pages/Dashboard';
 import ExamCreate from './pages/ExamCreate';
 import RosterRegister from './pages/RosterRegister';
 import ExamDetail from './pages/ExamDetail';
+import ExamEnterCode from './pages/ExamEnterCode';
+import ExamEnterStudent from './pages/ExamEnterStudent';
+import ExamSession from './pages/ExamSession';
+import ExamDone from './pages/ExamDone';
 
 // 로그인된 사용자만 접근 가능한 라우트 보호
 function PrivateRoute({ children }) {
@@ -52,6 +56,12 @@ export default function App() {
             </PrivateRoute>
           }
         />
+
+        {/* 학생 응시 플로우 (인증 불필요) */}
+        <Route path="/exam" element={<ExamEnterCode />} />
+        <Route path="/exam/enter" element={<ExamEnterStudent />} />
+        <Route path="/exam/session" element={<ExamSession />} />
+        <Route path="/exam/done" element={<ExamDone />} />
       </Routes>
     </BrowserRouter>
   );
